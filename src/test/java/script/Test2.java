766bf63480eb410f5df4348e820b24f46023544c
Pavithra <Pavithra@DESKTOP-O0KTUNL>
2021-08-19 20:22:12 +0530

    this is my comment

---
+++ src/test/java/script/Test2.java
@@ -0,0 +1,13 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import generic.BaseTest;
+
+public class Test2 extends BaseTest{
+	@Test(priority = 2)
+	public void testB()
+	{
+		Reporter.log("testB",true);
+	}
+}
